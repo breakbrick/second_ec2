22e0b6e0663d6b7944dd85e2da35b12171ac914c
Zhuo Guobao <1801762@sit.singaporetech.edu.sg>
2021-10-19 10:08:49 +0800

    add arg checking

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
+import sys
 from time import sleep
 
-for i in range(1,51):
-    print("Generating file_" + str(i) + "...")
-    with open("/samba/enclave/verify_hash/request_hash/file_" + str(i) + ".txt", "w+") as out_file:
-        out_file.write(str(i))
-    sleep(0.5)
+
+if sys.argv[1] == "sleep":
+    for i in range(1,51):
+        print("Generating file_" + str(i) + "...")
+        with open("/samba/enclave/verify_hash/request_hash/file_" + str(i) + ".txt", "w+") as out_file:
+            out_file.write(str(i))
+        sleep(0.5)
+elif sys.argv[1] == "nosleep":
+    for i in range(1,51):
+        print("Generating file_" + str(i) + "...")
+        with open("/samba/enclave/verify_hash/request_hash/file_" + str(i) + ".txt", "w+") as out_file:
+            out_file.write(str(i))
