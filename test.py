6118a66d3d76f31261f1732f1408301a569bdcc4
Zhuo Guobao <1801762@sit.singaporetech.edu.sg>
2021-10-19 11:36:03 +0800

    change generate files to class

--- test.py
+++ test.py
@@ -3,68 +3,70 @@ import os
 import sys
 from time import sleep
 
-parser = argparse.ArgumentParser("Generate x number of files for testing.") 
+class GenerateFiles():
+    """Class to handle generation of files
+    """
 
-# Add the arguments
-parser.add_argument('--process',
-                    metavar="HASH_GEN or VERIFY_HASH",
-                    type=str,
-                    required=True,
-                    help='process to take: can be either HASH_GEN or VERIFY_HASH')
+    def __init__(self, action, num_of_files, delay):
+        self.action = action
+        self.num_of_files = num_of_files
+        self.delay = delay
 
-parser.add_argument('--numOfFiles',
-                    metavar="NUM_OF_FILES",
-                    type=int,
-                    required=True,
-                    help='number of files to generate')
+    def generate_files(self):
+        if self.delay is None:
+            if self.action == "HASH_GEN":
+                for i in range(0,self.num_of_files):
+                    print("Generating file_" + str(i+1) + "...")
+                    with open("/samba/enclave/hash_generation/file_" + str(i+1) + ".txt", "w+") as out_file:
+                        out_file.write(str(i+1))
+            elif self.action == "VERIFY_HASH":
+                for i in range(0,self.num_of_files):
+                    print("Generating file_" + str(i+1) + "...")
+                    with open("/samba/enclave/verify_hash/request_hash/file_" + str(i+1) + ".txt", "w+") as out_file:
+                        out_file.write(str(i+1))
+        else:
+            if self.action == "HASH_GEN":
+                for i in range(0,self.num_of_files):
+                    print("Generating file_" + str(i+1) + "...")
+                    with open("/samba/enclave/hash_generation/file_" + str(i+1) + ".txt", "w+") as out_file:
+                        out_file.write(str(i+1))
+                    sleep(self.delay)
+            elif self.action == "VERIFY_HASH":
+                for i in range(0,self.num_of_files):
+                    print("Generating file_" + str(i+1) + "...")
+                    with open("/samba/enclave/verify_hash/request_hash/file_" + str(i+1) + ".txt", "w+") as out_file:
+                        out_file.write(str(i+1))
+                    sleep(self.delay)
 
-parser.add_argument('--delay',
-                    metavar="TIME_IN_SECONDS",
-                    type=float,
-                    help='delay in generating one file, in seconds')
 
-# Execute the parse_args() method
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Generate x number of files for testing.") 
 
-input_action = args.process
-input_num_files = args.numOfFiles
-input_delay = args.delay
+    # Add the arguments
+    parser.add_argument('--process',
+                        metavar="HASH_GEN or VERIFY_HASH",
+                        type=str,
+                        required=True,
+                        help='process to take: can be either HASH_GEN or VERIFY_HASH')
 
-if input_delay is None:
-    print("Generating files with no delay...")
-    if input_action == "HASH_GEN":
-        for i in range(1,input_num_files):
-            print("Generating file_" + str(i) + "...")
-            with open("/samba/enclave/hash_generation/file_" + str(i) + ".txt", "w+") as out_file:
-                out_file.write(str(i))
-    elif input_action == "VERIFY_HASH":
-        for i in range(1,input_num_files):
-            print("Generating file_" + str(i) + "...")
-            with open("/samba/enclave/verify_hash/request_hash/file_" + str(i) + ".txt", "w+") as out_file:
-                out_file.write(str(i))
-else:
-    print("Generating files with " + str(input_delay) + "s delay...")
-    if input_action == "HASH_GEN":
-        for i in range(1,input_num_files):
-            print("Generating file_" + str(i) + "...")
-            with open("/samba/enclave/hash_generation/file_" + str(i) + ".txt", "w+") as out_file:
-                out_file.write(str(i))
-        sleep(input_delay)
-    elif input_action == "VERIFY_HASH":
-        for i in range(1,input_num_files):
-            print("Generating file_" + str(i) + "...")
-            with open("/samba/enclave/verify_hash/request_hash/file_" + str(i) + ".txt", "w+") as out_file:
-                out_file.write(str(i))
-        sleep(input_delay)
+    parser.add_argument('--numOfFiles',
+                        metavar="NUM_OF_FILES",
+                        type=int,
+                        required=True,
+                        help='number of files to generate')
 
-# if sys.argv[1] == "sleep":
-#     for i in range(1,51):
-#         print("Generating file_" + str(i) + "...")
-#         with open("/samba/enclave/verify_hash/request_hash/file_" + str(i) + ".txt", "w+") as out_file:
-#             out_file.write(str(i))
-#         sleep(0.5)
-# elif sys.argv[1] == "nosleep":
-#     for i in range(1,51):
-#         print("Generating file_" + str(i) + "...")
-#         with open("/samba/enclave/verify_hash/request_hash/file_" + str(i) + ".txt", "w+") as out_file:
-#             out_file.write(str(i))
+    parser.add_argument('--delay',
+                        metavar="TIME_IN_SECONDS",
+                        type=float,
+                        help='delay in generating one file, in seconds')
+
+    # Execute the parse_args() method
+    args = parser.parse_args()
+
+    input_action = args.process
+    input_num_files = args.numOfFiles
+    input_delay = args.delay
+
+    # Call generate_files
+    generate_files_obj = GenerateFiles(input_action, input_num_files, input_delay)
+    generate_files_obj.generate_files()
