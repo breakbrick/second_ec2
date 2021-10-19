cd6eb304627fd44c0c82ad3bbe87fbf263dfe6b5
Zhuo Guobao <1801762@sit.singaporetech.edu.sg>
2021-10-19 09:12:45 +0800

    add sleep

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
+from time import sleep
+
 for i in range(1,11):
     print("Generating file_" + str(i) + "...")
     with open("/samba/enclave/verify_hash/request_hash/file_" + str(i) + ".txt", "w+") as out_file:
         out_file.write(str(i))
+    sleep(0.5)
