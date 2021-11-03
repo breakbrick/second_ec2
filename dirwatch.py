d2244a47b1367a3955bd7242d2341d9d97018732
Zhuo Guobao <1801762@sit.singaporetech.edu.sg>
2021-11-03 14:44:49 +0800

    change back to write hash value to file

--- dirwatch.py
+++ dirwatch.py
@@ -49,8 +49,8 @@ def process_func(event):
             hash_values = f.read()
         # Write the hash value to another folder
         with open("hash/hashes.txt", "a+") as t:
-            # t.write(hash_values + "\n")
-            t.write(" \n")
+            t.write(hash_values + "\n")
+            # t.write(" \n") # Test check on missing hash values
 
         check_hash_is_written(hash_values, event.src_path)
 
