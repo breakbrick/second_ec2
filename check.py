5e4995e749920aafae756cf1f4e925a10cdd9d7c
Zhuo Guobao <1801762@sit.singaporetech.edu.sg>
2021-11-03 17:56:00 +0800

    print the arg and file name

--- check.py
+++ check.py
@@ -1,7 +1,9 @@
 import time
 
 def check_hash_is_written(file_path_to_hash_values):
+    print(file_path_to_hash_values)
     file_name = file_path_to_hash_values[5]
+    print(file_name)
     with open("hash/hashes.txt", "r") as read_hashes:
         print("Checking if " + file_name[0:-9] + " is written to file hash storage ...")
         # Check if the file_name obtained is inside the hash file
