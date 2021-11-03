104ba2b781915a6a7f6fe454e5dabdcaac3e4db3
Zhuo Guobao <1801762@sit.singaporetech.edu.sg>
2021-11-03 16:14:40 +0800

    add if else to check for empty file contents

--- dirwatch.py
+++ dirwatch.py
@@ -47,21 +47,21 @@ def process_func(event):
         # Read the hash value from the shared folder
         with open(event.src_path, "r") as f:
             hash_values = f.read()
-            print("hash value: " + hash_values)
-            if hash_values == " " or hash_values == "":
-                print(event.src_path)
-                # sleep(10)
-                exit
-        # Write the hash value to another folder
-        with open("hash/hashes.txt", "a+") as t:
-            t.write(hash_values + "\n")
-            # t.write(" \n") # Test check on missing hash values
+        
+        if hash_values == "":
+            print("hash values: " + hash_values + str(event.src_path))
+        else:
 
-        # check_hash_is_written(hash_values, event.src_path)
+            # Write the hash value to another folder
+            with open("hash/hashes.txt", "a+") as t:
+                t.write(hash_values + "\n")
+                # t.write(" \n") # Test check on missing hash values
 
-        # print("[DIRWATCH] Removing " + str(event.src_path))
-        # # Remove the file in the shared folder
-        # remove(event.src_path)
+            # check_hash_is_written(hash_values, event.src_path)
+
+            print("[DIRWATCH] Removing " + str(event.src_path))
+            # Remove the file in the shared folder
+            remove(event.src_path)
     # Check if the 5th folder is request_hash
     elif action == "request_hash":
         print("[DIRWATCH] splitted ", splitted_file_path)
