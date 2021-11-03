2ae266c4d3c492e8ad8c1656c3cd21e31aed2fb8
Zhuo Guobao <1801762@sit.singaporetech.edu.sg>
2021-11-03 14:40:53 +0800

    amend print statement

--- check.py
+++ check.py
@@ -3,10 +3,11 @@ def check_hash_is_written(hash_values, file_path_to_hash_values):
         # Check if the file_name obtained is inside the hash file
         line = next((l for l in read_hashes if hash_values[65:] in l), None)
         if line is None:
-            print(hash_values[65:] + " is missing!")
+            print(">>>>>>>>>>>>>>> " + hash_values[65:] + " is missing!")
             # Read the file_path_to_hash_values
             with open(file_path_to_hash_values, "r") as to_write_hash_value:
                 missing_hash = to_write_hash_value.read()
+            # Write the missing hash values to file
             with open("hash/hashes.txt", "a+") as write_hashes:
                 write_hashes.write(missing_hash + "\n")
         else:
