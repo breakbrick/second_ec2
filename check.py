ad37f2310a7cdede64d452a6eeb9217f52b0520c
Zhuo Guobao <1801762@sit.singaporetech.edu.sg>
2021-11-03 17:57:52 +0800

    add split

--- check.py
+++ check.py
@@ -2,7 +2,7 @@ import time
 
 def check_hash_is_written(file_path_to_hash_values):
     print(file_path_to_hash_values)
-    file_name = file_path_to_hash_values[5]
+    file_name = file_path_to_hash_values.split("/")[5]
     print(file_name)
     with open("hash/hashes.txt", "r") as read_hashes:
         print("Checking if " + file_name[0:-9] + " is written to file hash storage ...")
