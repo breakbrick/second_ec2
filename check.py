6f4a74b884fa9c5021d6984de779997b348a0d5c
Zhuo Guobao <1801762@sit.singaporetech.edu.sg>
2021-11-05 11:42:18 +0800

    remove unused codes

--- check.py
+++ check.py
@@ -8,13 +8,6 @@ def check_hash_is_written(file_path_to_hash_values):
         line = next((l for l in read_hashes if file_name[0:-9] in l), None)
         if line is None:
             print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " + file_name[0:-9] + " is missing!")
-            # # Read the file_path_to_hash_values
-            # with open(file_path_to_hash_values, "r") as to_write_hash_value:
-            #     missing_hash = to_write_hash_value.read()
-            # # Write the missing hash values to file
-            # with open("hash/hashes.txt", "a+") as write_hashes:
-            #     write_hashes.write(missing_hash + "\n")
-            # time.sleep(10)
             return False
         else:
             print(file_name[0:-9] + " hash value is written!")
