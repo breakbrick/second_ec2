b3b3bc1cc5744c687e4e2c38e35fe2f373a29062
Zhuo Guobao <1801762@sit.singaporetech.edu.sg>
2021-12-01 11:30:47 +0800

    Revert "comment print of file path and name"
    This reverts commit 4b39351b79ff9ad968dde0091f642d8877a19451.

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 def check_hash_is_written(file_path_to_hash_values):
-    # print(file_path_to_hash_values)
+    print(file_path_to_hash_values)
     file_name = file_path_to_hash_values.split("/")[5]
-    # print(file_name)
+    print(file_name)
     with open("hash/hashes.txt", "r") as read_hashes:
         # print("Checking if " + file_name[0:-9] + " is written to file hash storage ...")
         # Check if the file_name obtained is inside the hash file
@@ -10,5 +10,5 @@ def check_hash_is_written(file_path_to_hash_values):
             print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " + file_name[0:-9] + " is missing!")
             return False
         else:
-            # print(file_name[0:-9] + " hash value is written!")
+            print(file_name[0:-9] + " hash value is written!")
             return True
